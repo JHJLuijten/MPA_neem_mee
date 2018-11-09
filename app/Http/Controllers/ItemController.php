361160b99351eaab0d43b28197272a6314f0edd1
JASPER-PC\Gebruiker <99027698@mydavinci.nl>
2018-11-09 13:42:27 +0100

    added commands to functions

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -9,16 +9,31 @@ use App\Suitcase;
 
 class ItemController extends Controller
 {
+
+    /**
+     * index shows all items
+     */
+
     function index(){
         $items=item::get();
         return view('/item',['items'=>$items]);
     }
+
+    /**
+     * shows a speific item
+     */
 
     function show($id){
         $item = item::find($id);
         $test = session::get('suitcase');
         return view('/show',['item'=>$item, 'test' => $test]);
     }
+
+    /**
+     * Adds an item to the suitcase
+     */
+
+    //sesions op juiste plek zetten
     public function addItem(Request $request, $id){
         $item = Item::find($id); 
         $usedSuitcase = Session::has('suitcase') ? Session::get('suitcase') : null;
@@ -27,6 +42,11 @@ class ItemController extends Controller
         $request->session()->put('suitcase' , $suitcase);
         return redirect()->back();
     }
+
+    /**
+     * Gets items that are in the suitcase
+     */
+
     public function getSuitcase(){
         $suitcase = new suitcase;
         $getSuitcase = $suitcase->getSuitcase();
--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -7,15 +7,23 @@ use App\Category;
 
 class CategoryController extends Controller
 {
+
+    /**
+     * shows all categories
+     */
+
     function categories(){
         $categories=category::all();
         
         return view('/categories',['categories'=>$categories]);
     }
+
+    /**
+     * show an category with the items that are in it
+     */
     function show($id){
         $items=Category::find($id)->items()->get();
-
-
+        
         return view('/show',['items'=>$items]);
     }
     

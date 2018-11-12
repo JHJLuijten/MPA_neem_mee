187dddf36cb4e946ec3a9185057d124932fcad6d
JASPER-PC\Gebruiker <99027698@mydavinci.nl>
2018-11-12 18:20:09 +0100

    in trello alle features van de backlogg uitgeschreven

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,4 @@ class Category extends Model
     public function items() {
       return $this->belongsToMany('App\Item', 'items_categories', 'category_id')->withPivot('category_id','item_id');
   }
-}
+} 
